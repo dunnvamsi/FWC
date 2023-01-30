a57836d661983058d493d62c9f051280fad7dda4
dunnvamsi <119913502+dunnvamsi@users.noreply.github.com>
2023-01-30 12:42:36 +0530

    Add files via upload

--- vector/12/10.5/5/codes/vec.py
+++ vector/12/10.5/5/codes/vec.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 # Define the vector
-a = np.array([1, 1, 1])
+x = np.array([1, 1, 1])
 
 # Find the magnitude of the vector
-c1 = (np.linalg.norm(a))
-A=1/(c1**a)
-print(A)
+c1 = (np.linalg.norm(x))
+X=1/(c1**x)
+print(X)
