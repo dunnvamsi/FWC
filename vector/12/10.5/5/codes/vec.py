c5a8fbce789c2e7aac6a1ef41a35da490653f5f2
dunnvamsi <119913502+dunnvamsi@users.noreply.github.com>
2023-01-31 10:57:08 +0530

    Add files via upload

--- vector/12/10.5/5/codes/vec.py
+++ vector/12/10.5/5/codes/vec.py
@@ -1,11 +1,6 @@
 import numpy as np
-import sympy as sym
-x=sym.Symbol('x')
-X=np.array([x,x,x])
-print(X)
-p=X@(X.T)-1
-print("{} =0".format(p))
-coeff = [3, 0,-1]
-r=np.roots(coeff)
-print("The value of x is : ")
-print(r)
+x=np.array(([1,1,1]))
+y=x@(x.T)
+print(y)
+z=1/np.sqrt(y)
+print(z)
